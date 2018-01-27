5269f804527b46d5c857f3931a902240933adcb4
davidmfinol <davidmfinol@gmail.com>
2018-01-27 00:08:14 -0600

    Add Sound Wave

---
+++ Assets/Scripts/SoundSource.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class SoundSource : MonoBehaviour
+{
+    public GameObject soundPrefab;
+    public Vector3 startPosition = new Vector3(-2.5f, 2, 5);
+    public Vector3 startLookDirection = new Vector3(0, 0, -1);
+    public float initialIntensity = 1.0f;
+    public Vector3 initialVelocity = new Vector3(0, 0, -10f);
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Submit"))
+            StartSoundWave();
+    }
+
+    public void StartSoundWave()
+    {
+        Quaternion startRotation = Quaternion.LookRotation(startLookDirection);
+        GameObject newSoundObject = Instantiate(soundPrefab, startPosition, startRotation);
+        SoundWave soundWave = newSoundObject.GetComponent<SoundWave>();
+        soundWave.GetComponent<Rigidbody>().velocity = initialVelocity;
+        soundWave.intensity = initialIntensity;
+    }
+}
---
+++ Assets/Scripts/SoundWave.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class SoundWave : MonoBehaviour
+{
+    public float intensity;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        ContactPoint contact = collision.contacts[0];
+        float dot = Vector3.Dot(contact.normal, (-transform.forward));
+        dot *= 2;
+        Vector3 reflection = contact.normal * dot;
+        reflection = reflection + transform.forward;
+        Vector3 velocity = reflection.normalized * 15.0f;
+        GetComponent<Rigidbody>().velocity = velocity;
+    }
+}
