7109fe1469c2ffdee1829e612c4d8f9b1eb1a541
DESKTOP-HV3NNI0\dp <dunskat@gmail.com>
2018-01-27 13:44:34 -0600

    Added enemy controller with annoyance level bar.

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour {
+
+    private float AnnoyanceLevel = 0;
+    [SerializeField]
+    private GameObject healthBar;
+    [SerializeField]
+    private Renderer healthRenderer;
+
+    [SerializeField]
+    private Color[] healthBarColors;
+
+    private Material healthMaterial;
+
+        private void Awake()
+    {
+        healthMaterial = healthRenderer.materials[0];
+
+        Vector3 healthScale = healthBar.transform.localScale;
+        healthScale.z = AnnoyanceLevel;
+        healthBar.transform.localScale = healthScale;
+
+        healthMaterial.color = Color.Lerp(healthBarColors[0], healthBarColors[1], AnnoyanceLevel);
+    }
+
+    private void Update()
+    {
+        //FOR TESTING ONLY
+        if (Input.GetMouseButtonDown(0))
+        {
+            UpdateAnnoyanceLevel(0.1f);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject hittingObj = collision.gameObject;
+        SoundWave soundWave = hittingObj.GetComponent<SoundWave>();
+        if (soundWave != null)
+        {
+            Destroy(hittingObj);
+            UpdateAnnoyanceLevel(soundWave.intensity);
+        }
+    }
+
+    private void UpdateAnnoyanceLevel(float intensity)
+    {
+        AnnoyanceLevel = Mathf.Clamp01(AnnoyanceLevel + intensity);
+        Debug.Log("Bad Guy Annoyed " + AnnoyanceLevel + " much");
+        Vector3 healthScale = healthBar.transform.localScale;
+        healthScale.z = AnnoyanceLevel;
+        healthBar.transform.localScale = healthScale;
+
+        healthMaterial.color = Color.Lerp(healthBarColors[0], healthBarColors[1], AnnoyanceLevel);
+    }
+}
