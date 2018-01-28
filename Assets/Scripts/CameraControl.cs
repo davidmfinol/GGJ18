8165725dfc20fdce0f4603aa5b8829b1051421bd
davidmfinol <davidmfinol@gmail.com>
2018-01-28 00:50:27 -0600

    Add camera control

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public float movementIncrement = 1f;
+
+    void Update()
+    {
+        if (!GameProgressionManager.instance.isInGameMode || GameProgressionManager.instance.IsGamePaused)
+            return;
+
+        if (Input.GetButtonDown("Horizontal")) {
+            if (Input.GetAxis("Horizontal") > 0)
+                transform.position = Vector3.right * movementIncrement + transform.position;
+            else
+                transform.position = Vector3.left * movementIncrement + transform.position;
+        } else if (Input.GetButtonDown("Vertical")) {
+            if (Input.GetAxis("Vertical") > 0)
+                transform.position = Vector3.forward * movementIncrement + transform.position;
+            else
+                transform.position = Vector3.back * movementIncrement + transform.position;
+        }
+    }
+}
